06f7ad05884c6347cd7367abd29b733fb17ebc63
Jim Lengel <jimlengel@gmail.com>
2016-05-25 19:32:46 -0700

    Add final question logic

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,3 +1,5 @@
+# Jim Lengel and Jacob Golding worked on this project together on 4/25/16.
+
 puts "What is the hamster's name?"
 name = gets.chomp
 
@@ -5,11 +7,13 @@ check = true
 while check
 	puts "What is the volume level? (1-10)"
 	volume = gets.chomp
-	volume.to_i
-	if volume > 0
-		check == false
-	elsif volume > 10
-		puts "That numbers is too large."
+	volume2 = volume.to_i
+	if volume2 > 10
+		puts "That number is too large."
+	elsif volume2 > 0
+		check = false
+	elsif volume2 < 0
+		puts "That number is too small."
 	else
 		puts "That is not a number."
 	end		
@@ -18,10 +22,18 @@ end
 puts "What color is the fur?"
 fur_color = gets.chomp
 
-adoption_ready = ""
-while adoption_ready != "y" || "n"
+check2 = true
+while check2
 	puts "Is the hamster a good candidate for adoption? y/n"
 	adoption_ready = gets.chomp
+
+	if adoption_ready == "y"
+		check2 = false
+	elsif adoption_ready == "n"
+		check2 = false			
+	else 
+		puts "Sorry, I did not understand."
+	end
 end
 
 puts "What is the estimated age of the hamster?"
