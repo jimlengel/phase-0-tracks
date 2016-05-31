8675742ea4f5995927bc24b9305fdd52db6e191c
Jim Lengel <lengel@MS-Tech-Pro.local>
2016-05-30 18:21:01 -0700

    Clean up code

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,16 +1,3 @@
-# In phase-0-tracks/ruby/hashes.rb, pseudocode and write a program that will allow an interior designer to enter the details of a given client: the client's name, age, number of children, decor theme, and so on (you can choose your own as long as it's a good mix of string, integer, and boolean data).
-
-# Your keys should be symbols unless you find that you need a string for some reason -- usually only when spaces or other "user friendly" formatting are needed (as in the case of using someone's full name as a hash key). Basically, symbols are simpler for programmers to use, and readable enough by technical folks to be used in place of strings most of the time.
-
-# Your program should ...
-
-# Prompt the designer/user for all of this information.
-# Convert any user input to the appropriate data type.
-# Print the hash back out to the screen when the designer has answered all of the questions.
-# Give the user the opportunity to update a key (no need to loop, once is fine). After all, sometimes users make mistakes! If the designer says "none", skip it. But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
-# Print the latest version of the hash, and exit the program.
-# Be sure to pseudocode, and leave your pseudocode in as comments.
-
 # Pseudocode
 # 1. Ask the designer for the client information:  the client's name, age, number of children, decor theme, if they pay using a check (true or false)
 # 2. Store the information in a hash.
@@ -93,6 +80,7 @@ if change_fields == 'y'
       client_details[:decor_theme] = new_value
     when "check"
       client_details[:prefer_check] = new_value
+      
       if client_details[:prefer_check] == "y"
         client_details[:prefer_check] = true
       else
@@ -108,6 +96,7 @@ if change_fields == 'y'
     puts "Age: " << client_details["age"]
     puts "Number of children: " << client_details["number_of_children"]
     puts "Decor theme: " << client_details[:decor_theme]
+
     if client_details[:prefer_check] == true
       puts "Client prefers to pay by check."
     else
