0e17860f009d5af665d4c792fc6311585f048fe3
Jim Lengel <jimlengel@gmail.com>
2016-06-02 20:54:49 -0700

    Release 2 with arrays only

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -40,5 +40,39 @@ end
 puts "After .map! call: "
 p warriors_positions # array
 
+# A method that iterates through the items, 
+# deleting any that meet a certain condition 
+# (for example, deleting any numbers that are less than 5).
 
+less_five = [5, 6, 7, 1, 2, 4]
+less_five.delete_if { |num| num < 5 }
+puts less_five
+
+
+
+# A method that filters a data structure for only items that 
+# do satisfy a certain condition 
+# (for example, keeping any numbers that are less than 5).
+
+puts " "
+filter_less_five = [5, 6, 7, 1, 2, 4]
+filter_less_five.keep_if { |num| num < 5 }
+puts filter_less_five
+
+# A different method that filters a data structure for 
+# only items satisfying a certain condition -- 
+# Ruby offers several options!
+
+puts " "
+thing_three = [10,12,13,14,25,46,67]
+thing_three.select! { |numb| numb.even? }
+puts thing_three
+
+# A method that will remove items from a data structure 
+# until the condition in the block evaluates to false, then stops.
+
+puts " "
+thing_four = [10,20,30,40,50,60,70]
+less_than_forty = thing_four.take_while { |i| i < 40 }
+puts less_than_forty
 
