768648693764ae5832d35f32a74c2a05014dfd9a
Jim Lengel <lengel@MS-Tech-Pro.local>
2016-06-06 19:27:03 -0700

    Create Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,25 @@
+# SPECIES ---
+# Canus domesticus
+
+# CHARACTERISTICS ---
+# Eyelash length: long
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: brown
+
+# BEHAVIOR ---
+# Run
+# Jump
+# Fetch
+# Bark
+
+class Puppy
+end
+
+p Puppy.methods
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
