cdc49c0a1b52b208ee2f8f681244cb9e62fc0fc8
Jim Lengel <lengel@MS-Tech-Pro.local>
2016-06-07 17:49:22 -0700

    Loop through the array to create instances

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -21,7 +21,26 @@ class Santa
 	
 end
 
-santa = Santa.new
 
-santa.eat_milk_and_cookies("snickerdoodle")
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
+santas = []
+genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+genders.length.times do |a|
+	puts genders[a]
+	puts ethnicities[a]
+	# puts "This santa's gender is #{genders[i]}."
+	# puts "This santa's ethnicity is #{ethnicities[i]}."
+	# santas << Santa.new(genders[i], ethnicities[i])
+	# puts "There are now #{genders.length} Santa instances."
+	# puts " "
+end
 
