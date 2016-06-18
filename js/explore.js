69730a3f0976bdc42d4cfd76dd3cca4139af48b0
Jim Lengel <jimlengel@gmail.com>
2016-06-18 10:09:27 -0700

    write sentence at the end if the two strings are reversed

--- js/explore.js
+++ js/explore.js
@@ -1,30 +1,36 @@
 // pseudocode a function that takes a string as a parameter and reverses the string.
 //
-// count the number of characters in the string (.length)
-// loop through the string, starting with the last character of the variable
-// write the last character to the first character of a new variable
-// check if there are more characters. if so, continue on the same path.
-// when there are no more characters, the string is complete; it has been reversed.
+// 1. count the number of characters in the string (.length)
+// 2. loop through the string, starting with the last character of the variable
+// 3. write the last character to the first character of a new variable
+// 4. check if there are more characters. if so, continue on the same path.
+// 5. when there are no more characters, the string is complete; it has been reversed.
+// then...
+// 1. create a varaible with a reversed string
+// 2. create a new variable that reverses the reversed string
+// 3. compare the two variables
+// 4. if they are the same, print the new variables and that they are equal.
 
 function reverseString(string){
 	stringLength = string.length;
-	//console.log(stringLength);
-	var new_string = ""
-	var reverse_string = ""
+	var newString = ""
+	var reverseString = ""
 
 	for (var i = stringLength; i >= 0; i--) {
-    //console.log(i);
-    //console.log(string.slice(i));
-    new_string = string.slice(i);
-    new_string = new_string.charAt(0) 
-    //console.log("character: " + new_string);
-    reverse_string += new_string;
-    //console.log("reversed: " + reverse_string);
+    newString = string.slice(i);
+    newString = newString.charAt(0); 
+    reverseString += newString;
 	}
-	//console.log("reversed: " + reverse_string);
-	return reverse_string;
+	return reverseString;
 }
 
-var reverseIt = reverseString('happy');
+var reverseableWord = 'javascript';
+var reverseIt = reverseString(reverseableWord);
+var reverseItTwice = reverseString(reverseString(reverseableWord));
 
-console.log(reverseIt);
+//console.log(reverseIt);
+//console.log(reverseItTwice);
+
+if (reverseableWord === reverseItTwice){
+	console.log("The word" + reverseableWord + " is " + reverseIt + " reversed.");
+} 
