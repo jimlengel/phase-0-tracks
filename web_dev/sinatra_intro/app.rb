4214c14130a1387efb544f32646b8f22f0b1420d
Jim Lengel <jimlengel@gmail.com>
2016-06-30 12:39:26 -0700

    release 0, 1-3

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -7,21 +7,69 @@ db.results_as_hash = true
 
 # write a basic GET route
 # add a query parameter
-# GET /
-get '/' do
-  "#{params[:name]} is #{params[:age]} years old."
-end
+#GET /
+
+# get '/' do
+#   "#{params[:name]} is #{params[:age]} <b>years</b> old."
+# end
+
+# get '/' do
+#   name=params[:name]
+#   if name
+#    "Hello, #{name}."
+#   else
+#     "Hello, you."
+#   end
+# end
+
 
 # write a GET route with
 # route parameters
-get '/about/:person' do
-  person = params[:person]
-  "#{person} is a programmer, and #{person} is learning Sinatra."
+
+# get '/about/:person' do
+#   person = params[:person]
+#   "#{person} is a programmer, and #{person} is learning Sinatra."
+# end
+
+# A /contact route that displays an address (you can make up the address).
+get '/contact/:address' do
+  address = params[:address]
+  "The address is: #{address}."
 end
 
-get '/:person_1/loves/:person_2' do
-  "#{params[:person_1]} loves #{params[:person_2]}"
+# A /great_job route that can take a person's name as a query parameter 
+# (not a route parameter) and say "Good job, [person's name]!". 
+# If the query parameter is not present, the route simply says "Good job!"
+get '/great_job/' do
+  person = params[:person]
+  if person
+    "Good job, #{person}!"
+  else
+    "Good job!"
+  end
 end
+
+# A route that uses route parameters to add two numbers and respond with the result. 
+# The data types are tricky here -- when will the data need to be (or arrive as) a string?
+get '/add/:num1/:num2' do
+  num1 = params[:num1]
+  num2 = params[:num2]
+  sum = num1.to_i+num2.to_i
+  "#{num1} + #{num2} = #{sum}"
+  
+end
+
+
+
+# get '/contact/:address' do
+#   "The address is: #{params[:address]}."
+# end
+
+# get
+
+# get '/:person_1/loves/:person_2' do
+#   "#{params[:person_1]} loves #{params[:person_2]}"
+# end
 
 # write a GET route that retrieves
 # all student data
